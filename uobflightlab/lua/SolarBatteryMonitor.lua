dceadc5823b0c1e57b9481585c04d61cbd79c738
Robert Clarke <robert.clarke@bristol.ac.uk>
2025-10-31 13:06:36 +0000

    Add code for second battery

--- uobflightlab/lua/SolarBatteryMonitor.lua
+++ uobflightlab/lua/SolarBatteryMonitor.lua
@@ -1,7 +1,6 @@
 local function get_instance_power_W(instance)
     local v = battery:voltage(instance)
     local i = battery:current_amps(instance) or 0
-    gcs:send_text(6, string.format("Instance %i: V=%f I=%f", instance, v, i))
     return v * i
 end
 
@@ -11,23 +10,34 @@ local function get_battery_output_W(output_instance, input_instance)
     return output_W - input_W
 end
 
-local batt_consumed_J = 0.0
-
 local last_read_time_ms = millis()
+
+local function compute_consumption_J(output_power_W, time_delta_ms)
+    return output_power_W * time_delta_ms:tofloat() / 1000.0
+end
+
+local batt1_consumed_J = 0.0
+local batt2_consumed_J = 0.0
 
 function update()
     -- Get the power for the configured batteries
     local now_ms = millis()
-    local total_output_W = get_battery_output_W(2, 0)
-    gcs:send_text(6, string.format("Total power: %f", total_output_W))
+    local total_output_1_W = get_battery_output_W(2, 0)
+    local total_output_2_W = get_battery_output_W(2, 0)
+    gcs:send_text(6, string.format("Total power 1: %f", total_output_1_W))
+    gcs:send_text(6, string.format("Total power 2: %f", total_output_2_W))
 
     -- Update the estimated energy consumed for each battery
-    batt_consumed_J = batt_consumed_J + total_output_W * (now_ms - last_read_time_ms):tofloat() / 1000.0
+    local time_delta_ms = now_ms - last_read_time_ms
+    batt1_consumed_J = batt1_consumed_J + compute_consumption_J(total_output_1_W, time_delta_ms)
+    batt2_consumed_J = batt2_consumed_J + compute_consumption_J(total_output_2_W, time_delta_ms)
     last_read_time_ms = now_ms
 
     -- Send a debug message with the values
-    -- gcs:send_named_float("BAT1_J_CON", batt_consumed_J)
-    gcs:send_text(6, string.format("BAT1_J_CON: %f", batt_consumed_J))
+    -- gcs:send_named_float("BAT1_J_CON", batt1_consumed_J)
+    -- gcs:send_named_float("BAT2_J_CON", batt2_consumed_J)
+    gcs:send_text(6, string.format("BAT1_J_CON: %f", batt1_consumed_J))
+    gcs:send_text(6, string.format("BAT2_J_CON: %f", batt2_consumed_J))
 
     return update, 1000 -- reschedule at 1Hz
 end
